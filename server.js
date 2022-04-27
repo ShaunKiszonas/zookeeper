07c4e96525a589f2004db4a59c8a451766d4b817
Shaun Kiszonas <smkiszonas@gmail.com>
2022-04-27 14:35:48 -0700

    idk

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { animals } = require('./data/animals.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
