830764f5f986b54d8b733c510ac6612a4a7c3584
Shaun Kiszonas <smkiszonas@gmail.com>
2022-04-27 14:23:03 -0700

    Add Heroku

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { animals } = require('./data/animals.json');
+
+const app = express();
+const PORT = 3001;
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+        })
+    }
+
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}/api/animals`);
+});
